ebb2855688ae90eea12457b6544e7cceda635723
JhofreZambrano <56734629+JhofreZambrano@users.noreply.github.com>
2019-10-18 20:45:59 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,61 +1,21 @@
 ﻿using System;
 
-namespace programa10
+namespace programa9
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int num, a, c = 0, f = 1, ban = 1;
-            while (ban == 1)
+            int c = 0, num, mul;
+            Console.WriteLine("Ingrese numero número de cual desee la tabla ");
+            num = int.Parse(Console.ReadLine());
+            do
             {
-                Console.WriteLine(" MENÚ ");
-                Console.WriteLine(" 1) Salir ");
-                Console.WriteLine(" 2) Sumatoria ");
-                Console.WriteLine(" 3) Factorial ");
-                Console.WriteLine("Que accion desea realizar");
-                num = int.Parse(Console.ReadLine());
-                if (num == 1)
-                {
-                    Console.WriteLine("Gracias por su Visita");
-                    Console.ReadLine();
-                }
-                else
-                {
-                    if (num == 2)
-                    {
-                        Console.WriteLine("Ingrese Numero para realizar su Sumatorio ");
-                        a = int.Parse(Console.ReadLine());
-                        for (int i = 0; i <= a; i++)
-                        {
-                            c = c + i;
-                        }
-                        Console.WriteLine("El sumatorio del número ingresado es " + c);
-                        Console.ReadLine();
-                    }
-                    else
-                    {
-                        if (num == 3)
-                        {
-                            Console.WriteLine("Ingrese Numero para resolver su factorial");
-                            a = int.Parse(Console.ReadLine());
-                            for (int i = 1; i <= a; i++)
-                            {
-                                f = f * i;
-                            }
-                            Console.WriteLine("El factorial del numero ingresado es" + f);
-                            Console.ReadLine();
-                        }
-                        else
-                        {
-                            Console.WriteLine("Accion no Valida ");
-                            Console.ReadLine();
-
-                        }
-                    }
-
-                }
-            }
+                c++;
+                mul = num * c;
+                Console.WriteLine("La multimplicacion de {0}x{1} ={2} ", c, num, mul);
+            } while (c < 15);
+            Console.ReadLine();
         }
     }
 }
